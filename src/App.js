a9e95feec456a26c42205341e4eff53a5e857268
unknown <hamiltonjlewis@gmail.com>
2020-11-05 12:02:10 -0600

    added video

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    	<div className='App'>
+			<header className='App-header'>
+				<ReactPlayer
+					playing
+					width='100%'
+					height='100%'
+					controls='true'
+					url={[{ src: 'movie.mp4', type: 'video/mp4' }]}
+				/>
+			</header>
+		</div>
   );
 }
 
